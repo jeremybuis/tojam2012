c8f580e16ee8f021c954b21eadad36529a2ec299
Jeremy Buis <buis.jeremy@gmail.com>
2012-05-12 15:13:54 -0400

    updated the server code a bit - chagned the arch.  Added more of my info to humans

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 //done this way to include the static content in client
 //wasnt able to figure out how to '../' when getting the path name for static
-var server = require('./server/server').createServer(__dirname);
+var server = require('./server/server').createServer(__dirname, 1337);
--- server/server.js
+++ server/server.js
@@ -1,64 +1,126 @@
+var util = require('util');
+
 var express = require('express');
-var io = require('socket.io');
+var sio = require('socket.io');
 
-var playerList = [];
+var colours = {
+	red: "#FF0000",
+	blue: "#0000FF",
+	green: "#008000",
+	yellow: "FFFF00",
 
-function Server() {
-	this.app = null;
-	this.sio = null;
+	white: "#FFFFFF",
+	black: "#000000",
+
+	silver: "#C0C0C0",
+	grey: "#808080"
 };
 
-Server.prototype.createServer = function(directory) {
+var client_event_types = {
+	pos: 'POS',
+	bullet: 'BULLET',
+	bullet_death: 'BULLET_DEATH',
+	death: 'DEATH',
+
+	//these are implicit
+	conn: 'connection',
+	disconn: 'disconnect'
+};
+
+var server_event_types = {
+	death: 'DEATH',
+	pos: 'POS',
+	join: 'JOIN',
+	conn: 'CONN',
+	disconn: 'DISCONN',
+	bullet: 'BULLET',
+	bullet_death: 'BULLET_DEATH'
+};
+
+
+//
+// Our server class
+//
+//
+function Server() {
+	this.app = null;
+	this.io = null;
+
+	this.clients = null;
+}
+
+Server.prototype.debug = function() {
+	//do something useful ;)
+};
+
+Server.prototype.createServer = function(directory, port) {
 	var that = this;
 
 	this.app = express.createServer();
 
 	this.app.configure(function() {
 		that.app.use(express.static(directory + '/client'));
-
-		that.app.use(express.bodyParser());
 		
-		that.app.use(express.cookieParser());
-		that.app.use(express.session({secret: 'secret', key: 'express.sid'}));
+		// that.app.use(express.bodyParser());
+		// that.app.use(express.cookieParser());
+		// that.app.use(express.session({secret: 'secret', key: 'express.sid'}));
 		// app.use(function(req, res) {
 		// 	res.end('<h2>Hello, your session id is ' + req.sessionID + '</h2>');
 		// });
 	});
+
+	this.clients = {};
+
+	this.app.listen(port);
+	this.io = sio.listen(this.app);
 };
 
-Server.prototype.listen = function(port) {
-	this.app.listen(1337);
-	this.sio = io.listen(this.app);
-}
+Server.prototype.handleClientEvents = function() {
+	var that = this;
 
-Server.prototype.handleConnections = function() {
-	
-	this.sio.sockets.on('connection', function (socket) {
-		//add players to state
-		// socket.emit('player connect', {
-		// 	hello: "world"
-		// });
+	//Connection/Disconnect code
+	this.io.sockets.on(client_event_types.conn, function (socket) {
+		util.log(client_event_types.conn);
 
-		//do something
+		that.clients[socket.id] = socket; //add the socket to our list of clients
+
+		socket.on(client_event_types.disconn, function() {
+			util.log(client_event_types.disconn);
+
+			delete that.clients[socket.id];
+		});
 	});
 
-	this.sio.sockets.on('disconnect', function (socket) {
-		//remove player from state
-		// socket.emit('player disconnect', {
-		// 	hello: "world"
-		// });
+	//Game event code
+	//POS
+	this.io.sockets.on(client_event_types.pos, function (socket) {
+		util.log(client_event_types.pos);
 
-		//do something
-	})
+		//emit the clients position to all the other clients
+	});
+
+	//BULLET
+	this.io.sockets.on(client_event_types.bullet, function (socket) {
+		util.log(client_event_types.bullet);
+	});
+
+	//BULLET_DEATH
+	this.io.sockets.on(client_event_types.bullet_death, function (socket) {
+		util.log(client_event_types.bullet_death);
+	});
+
+	//DEATH
+	this.io.sockets.on(client_event_types.death, function (socket) {
+		util.log(client_event_types.death);
+	});
 	
 };
 
-function createServer(dir) {
-	var srv = new Server()
+function createServer(dir, port) {
+	var srv = new Server();
 
-	srv.createServer(dir);
-	srv.listen(1337);
-	srv.handleConnections();
-};
+	srv.createServer(dir, port);
+	srv.handleClientEvents();
+}
 
 exports.createServer = createServer;
