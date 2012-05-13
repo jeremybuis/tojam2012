673fc4bfadacb0d952d2b12cdcbc011cb81c7425
Jeremy Buis <buis.jeremy@gmail.com>
2012-05-13 14:51:17 -0400

    updated the color to spec - started keeping track of kill/deaths

--- server/server.js
+++ server/server.js
@@ -25,22 +25,41 @@ var server_event_types = {
 
 var nextColor = (function() {
 	var colors = [
-		// {color: "black", code: "#000000"},
-		// {color: "white", code: "#FFFFFF"},
-		{color: "red", code: "#FF0000"},
-		{color: "lime", code: "#00FF00"},
-		{color: "blue", code: "#0000FF"},
-		{color: "yellow", code: "FFFF00"},
-		{color: "cyan", code: "#00FFFF"},
-		{color: "magenta", code: "#FF00FF"},
-		{color: "silver", code: "#C0C0C0"},
-		{color: "gray", code: "#808080"},
-		{color: "maroon", code: "#800000"},
-		{color: "olive", code: "#808000"},
-		{color: "green", code: "#008000"},
-		{color: "purple", code: "#800080"},
-		{color: "teal", code: "#008080"},
-		{color: "navy", code: "#000080"}
+		// // {color: "black", code: "#000000", rgb: {r:0, g:0, b:0}},
+		// // {color: "white", code: "#FFFFFF", rgb: {r:255, g:255, b:255}},
+		// {color: "red", hex: "#FF0000", rgb: {r:255, g:0, b:0}},
+		// {color: "lime", hex: "#00FF00", rgb: {r:0, g:255, b:0}},
+		// {color: "blue", hex: "#0000FF", rgb: {r:0, g:0, b:255}},
+		// {color: "yellow", hex: "FFFF00", rgb: {r:255, g:255, b:0}},
+		// {color: "cyan", hex: "#00FFFF", rgb: {r:0, g:255, b:255}},
+		// {color: "magenta", hex: "#FF00FF", rgb: {r:255, g:0, b:255}},
+		// {color: "silver", hex: "#C0C0C0", rgb: {r:192, g:192, b:192}},
+		
+		// {color: "gray", hex: "#808080", rgb: {r:128, g:128, b:128}},
+		// {color: "maroon", hex: "#800000", rgb: {r:128, g:0, b:0}},
+		// {color: "olive", hex: "#808000", rgb: {r:128, g:128, b:0}},
+		// {color: "green", hex: "#008000", rgb: {r:0, g:128, b:0}},
+		// {color: "purple", hex: "#800080", rgb: {r:128, g:0, b:128}},
+		// {color: "teal", hex: "#008080", rgb: {r:0, g:128, b:128}},
+		// {color: "navy", hex: "#000080", rgb: {r:0, g:0, b:128}}
+
+		// {color: "black", code: "#000000", rgb: {r:0, g:0, b:0}},
+		// {color: "white", code: "#FFFFFF", rgb: {r:255, g:255, b:255}},
+		{color: "red", hex: "#FF0000", rgb: {r:1, g:0, b:0}},
+		{color: "lime", hex: "#00FF00", rgb: {r:0, g:1, b:0}},
+		{color: "blue", hex: "#0000FF", rgb: {r:0, g:0, b:1}},
+		{color: "yellow", hex: "FFFF00", rgb: {r:1, g:1, b:0}},
+		{color: "cyan", hex: "#00FFFF", rgb: {r:0, g:1, b:1}},
+		{color: "magenta", hex: "#FF00FF", rgb: {r:1, g:0, b:1}},
+		{color: "silver", hex: "#C0C0C0", rgb: {r:0.75, g:0.75, b:0.75}},
+		
+		{color: "gray", hex: "#808080", rgb: {r:0.5, g:0.5, b:0.5}},
+		{color: "maroon", hex: "#800000", rgb: {r:0.5, g:0, b:0}},
+		{color: "olive", hex: "#808000", rgb: {r:0.5, g:0.5, b:0}},
+		{color: "green", hex: "#008000", rgb: {r:0, g:0.5, b:0}},
+		{color: "purple", hex: "#800080", rgb: {r:0.5, g:0, b:0.5}},
+		{color: "teal", hex: "#008080", rgb: {r:0, g:0.5, b:0.5}},
+		{color: "navy", hex: "#000080", rgb: {r:0, g:0, b:0.5}}
 	];
 
 	var start = 0;
@@ -69,7 +88,6 @@ var nextPosition = (function() {
 		var index = (start++)%positions.length;
 		return positions[index];
 	};
-
 })();
 
 //
@@ -92,7 +110,7 @@ var Ship = (function() {
 		this.health = 100;
 		this.fuel = 100;
 		this.weapon = 1;
-		this.color = nextColor().code;
+		this.color = nextColor().rgb;
 		this.kills = 0;
 		this.deaths = 0;
 	};
@@ -185,7 +203,14 @@ Server.prototype.onClientEvents = function() {
 		for (var client in that.clients) {
 			if (that.clients.hasOwnProperty(client)) {
 				if (socket.id !== client) {
-					that.clients[client].socket.emit(server_event_types.conn, that.clients[socket.id].ship);
+					var data = {
+						id: that.clients[socket.id].ship.id,
+						x:that.clients[socket.id].ship.x,
+						y:that.clients[socket.id].ship.y,
+						color: that.clients[socket.id].ship.color
+					};
+
+					that.clients[client].socket.emit(server_event_types.conn, data);
 					others.push(that.clients[client].ship);
 				}
 			}
@@ -204,33 +229,72 @@ Server.prototype.onClientEvents = function() {
 		//each client
 
 		//DISCONN
-		socket.on(client_event_types.disconn, function (data) {
+		socket.on(client_event_types.disconn, function () {
 			util.log(client_event_types.disconn);
 
-			that.emitServerEvent(server_event_types.disconn, socket, {id: that.clients[socket.id].ship.id});
+			var data = {
+				id: that.clients[socket.id].ship.id
+			};
+
+			that.emitServerEvent(server_event_types.disconn, socket, data);
 			//removes the socket.id from the clients object
 			delete that.clients[socket.id];
 		});
 
 		//POS
 		socket.on(client_event_types.pos, function (data) {
-			util.log(client_event_types.pos);
+			//util.log(client_event_types.pos);
 
 			//sync our data
-			// var currentShip = that.clients[socket.id].ship;
+			var currentShip = that.clients[socket.id].ship;
 
-			// currentShip.x = data.x;
-			// currentShip.y = data.y;
-			// currentShip.theta = data.theta;
-			// currentShip.vx = data.vx;
-			// currentShip.vy = data.vy;
-			// currentShip.health = data.health;
-			// currentShip.fuel = data.fuel;
-			// currentShip.weapon = data.weapon;
-			// currentShip.kills = data.kills;
-			// currentShip.deaths = data.deaths;
+			currentShip.x = data.x;
+			currentShip.y = data.y;
+			currentShip.theta = data.theta;
+			currentShip.vx = data.vx;
+			currentShip.vy = data.vy;
+			currentShip.health = data.health;
+			currentShip.fuel = data.fuel;
+			currentShip.weapon = data.weapon;
 
-			that.emitServerEvent(server_event_types.pos, socket, data);
+			//wrap our ship into an object to send out
+			var out = {
+				id: currentShip.id,
+				x: currentShip.x,
+				y: currentShip.y,
+				theta: currentShip.theta,
+				vx: currentShip.vx,
+				vy: currentShip.vy,
+				health: currentShip.health,
+				fuel: currentShip.fuel,
+				weapon: currentShip.weapon,
+				kills: currentShip.kills,
+				deaths: currentShip.deaths
+			};
+
+			that.emitServerEvent(server_event_types.pos, socket, out);
+		});
+
+		//DEATH
+		socket.on(client_event_types.death, function (data) {
+			util.log(client_event_types.death);
+
+			var dead = data.id;
+			var killer = data.killingId;
+
+			//update the kill death counters
+			for (var client in clients) {
+				if (that.clients.hasOwnProperty(client)) {
+					if (killer === clients[client].ship.id) {
+						clients[client].ship.kills += 1;
+					} else if (dead === clients[client].ship.id) {
+						clients[client].ship.deaths += 1;
+					}
+				}
+			}
+
+			//simply repeat out to the other clients that a player has died
+			that.emitServerEvent(server_event_types.death, socket, data);
 		});
 
 		//BULLET
@@ -245,13 +309,6 @@ Server.prototype.onClientEvents = function() {
 			util.log(client_event_types.bullet_death);
 
 			that.emitServerEvent(server_event_types.bullet_death, socket, data);
-		});
-
-		//DEATH
-		socket.on(client_event_types.death, function (data) {
-			util.log(client_event_types.death);
-
-			that.emitServerEvent(server_event_types.death, socket, data);
 		});
 	});
 };
