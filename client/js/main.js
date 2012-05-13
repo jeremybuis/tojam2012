c86124a3c478f12b60e6958ac490c82b046c9751
kwogger <kwogger@gmail.com>
2012-05-13 03:13:16 -0400

    Added stat tracking and rendering for ships.

--- client/js/main.js
+++ client/js/main.js
@@ -25,6 +25,8 @@ var FIELD_HEIGHT = 800;
 // Ship constants
 var SHIP_RADIUS = 50;
 var SHIP_MAX_ACCEL = 0.1;
+var MAX_STAT = 100;
+var START_STAT = 100;
 
 // Physics constants
 var TIME_CONST = 1;
@@ -41,7 +43,6 @@ Crafty.c('ship', {
 	init: function() {
 		if (!this.has('2D')) this.addComponent('2D');
 		if (!this.has('DOM')) this.addComponent('DOM');
-		if (!this.has('Color')) this.addComponent('Color');
 
 		this.attr({
 			h: SHIP_RADIUS,
@@ -50,15 +51,23 @@ Crafty.c('ship', {
 			vy: 0,
 			ax: 0,
 			ay: 0,
-			health: 100,
-			weapon: 100,
-			engine: 100,
+			engine: START_STAT,
+			hull: START_STAT,
+			weap: START_STAT,
 			rotation: -90
 		});
 
 		this.origin('center');
 
-		this.color('#fff');
+		this.ship_R = Crafty.e('2D, DOM, ship_R').attr({h: this.h, w: this.w}).origin('center');
+		this.ship_G = Crafty.e('2D, DOM, ship_G').attr({h: this.h, w: this.w}).origin('center');
+		this.ship_B = Crafty.e('2D, DOM, ship_B').attr({h: this.h, w: this.w}).origin('center');
+		this.engine_R = Crafty.e('2D, DOM, engine_R').attr({h: this.h, w: this.w}).origin('center');
+		this.engine_G = Crafty.e('2D, DOM, engine_G').attr({h: this.h, w: this.w}).origin('center');
+		this.hull_R = Crafty.e('2D, DOM, hull_R').attr({h: this.h, w: this.w}).origin('center');
+		this.hull_G = Crafty.e('2D, DOM, hull_G').attr({h: this.h, w: this.w}).origin('center');
+		this.weap_R = Crafty.e('2D, DOM, weap_R').attr({h: this.h, w: this.w}).origin('center');
+		this.weap_G = Crafty.e('2D, DOM, weap_G').attr({h: this.h, w: this.w}).origin('center');
 
 		this.bind("EnterFrame", function(e) {
 			this.vx = this.vx + this.ax * TIME_CONST;
@@ -77,7 +86,42 @@ Crafty.c('ship', {
 				this.y += FIELD_HEIGHT;
 			}
 			this.y = this.y + this.vy * TIME_CONST + TIME_CONST_SQU_HALF * this.ay;
+
+			this.ship_R.attr({x: this.x, y: this.y, rotation: this.rotation});
+			this.ship_G.attr({x: this.x, y: this.y, rotation: this.rotation});
+			this.ship_B.attr({x: this.x, y: this.y, rotation: this.rotation});
+			this.engine_R.attr({x: this.x, y: this.y, rotation: this.rotation, alpha: 1 - this.engine / 100});
+			this.engine_G.attr({x: this.x, y: this.y, rotation: this.rotation, alpha: this.engine / 100});
+			this.hull_R.attr({x: this.x, y: this.y, rotation: this.rotation, alpha: 1 - this.hull / 100});
+			this.hull_G.attr({x: this.x, y: this.y, rotation: this.rotation, alpha: this.hull / 100});
+			this.weap_R.attr({x: this.x, y: this.y, rotation: this.rotation, alpha: 1 - this.weap / 100});
+			this.weap_G.attr({x: this.x, y: this.y, rotation: this.rotation, alpha: this.weap / 100});
 		});
+
+		console.log(this);
+	},
+	setEngine: function (engine) {
+		if (this.engine != engine) {
+			this.engine = engine;
+			// TODO: update engine GFX
+		}
+	},
+	setHealth: function (health) {
+		if (this.health != health) {
+			this.health = health;
+			// TODO: update health GFX
+		}
+	},
+	setWeapon: function (weapon) {
+		if (this.weapon != weapon) {
+			this.weapon = weapon;
+			// TODO: update weapon GFX
+		}
+	},
+	setColor: function(r, g, b) {
+		this.ship_R.alpha = r * 0.5;
+		this.ship_G.alpha = g * 0.5;
+		this.ship_B.alpha = b * 0.5;
 	}
 });
 
@@ -124,7 +168,7 @@ Crafty.scene('ship_test', function() {
 	Crafty.e('ship, player').attr({
 		x: FIELD_WIDTH / 2,
 		y: FIELD_HEIGHT / 2
-	});
+	}).setColor(1, 1, 1);
 });
 
 window.onload = function() {
@@ -132,5 +176,3 @@ window.onload = function() {
 	Crafty.init(WINDOW_WIDTH, WINDOW_HEIGHT);
 	Crafty.scene('ship_test');
 };
-
-//Blarg
