8e8210d4a74074520ccc4faf4d638fb921bb27e1
kwogger <kwogger@gmail.com>
2012-05-13 13:18:56 -0400

    Added initial parsing and communication with server on the client.

--- client/js/main.js
+++ client/js/main.js
@@ -1,19 +1,3 @@
-(function(window, undefined) {
-
-	var socket = io.connect('http://localhost');
-			
-	socket.on('player connect', function (data) {
-		console.log('connection');
-		console.log(data);
-	});
-
-	socket.on('player disconnect', function (data) {
-		console.log('disconnection');
-		console.log(data);
-	});
-
-})(window);
-
 // CONSTANTS
 
 // Size of the playing field
@@ -24,6 +8,7 @@ var FIELD_HEIGHT = 800;
 
 // Ship constants
 var SHIP_RADIUS = 50;
+var BULLET_RADIUS = 5;
 var SHIP_MAX_ACCEL = 0.1;
 var MAX_STAT = 1;
 var START_STAT = 0.5;
@@ -33,18 +18,28 @@ var TIME_CONST = 1;
 var TIME_CONST_SQU_HALF = TIME_CONST * TIME_CONST / 2;
 var DEG_TO_RAD = Math.PI / 180;
 
+// Network constants
+var SHIP_UPDATE_RATE = 1;
+
 // Controls
 var CTRL_ACCEL = Crafty.keys.W;
 var CTRL_DECEL = Crafty.keys.S;
 var CTRL_TURN_CW = Crafty.keys.A;
 var CTRL_TURN_CCW = Crafty.keys.D;
 
+Crafty.c('socket', {
+	initSocket: function(socket) {
+		this.socket = socket;
+		return this;
+	}
+});
+
 Crafty.c('ship', {
 	init: function() {
-		if (!this.has('2D')) this.addComponent('2D');
-		if (!this.has('DOM')) this.addComponent('DOM');
+		this.requires('2D, DOM');
 
 		this.attr({
+			id: null,
 			h: SHIP_RADIUS,
 			w: SHIP_RADIUS,
 			vx: 0,
@@ -54,7 +49,9 @@ Crafty.c('ship', {
 			engine: START_STAT,
 			hull: START_STAT,
 			weap: START_STAT,
-			rotation: -90
+			rotation: -90,
+			kills: 0,
+			deaths: 0
 		});
 
 		this.origin('center');
@@ -69,7 +66,7 @@ Crafty.c('ship', {
 		this.weap_R = Crafty.e('2D, DOM, weap_R').attr({h: this.h, w: this.w}).origin('center');
 		this.weap_G = Crafty.e('2D, DOM, weap_G').attr({h: this.h, w: this.w}).origin('center');
 
-		this.bind("EnterFrame", function(e) {
+		this.bind('EnterFrame', function(e) {
 			this.vx = this.vx + this.ax * TIME_CONST;
 			this.vy = this.vy + this.ay * TIME_CONST;
 			this.x = this.x + this.vx * TIME_CONST + TIME_CONST_SQU_HALF * this.ax;
@@ -79,13 +76,13 @@ Crafty.c('ship', {
 			while (this.x < 0) {
 				this.x += FIELD_WIDTH;
 			}
+			this.y = this.y + this.vy * TIME_CONST + TIME_CONST_SQU_HALF * this.ay;
 			while (this.y > FIELD_HEIGHT) {
 				this.y -= FIELD_HEIGHT;
 			}
 			while (this.y < 0) {
 				this.y += FIELD_HEIGHT;
 			}
-			this.y = this.y + this.vy * TIME_CONST + TIME_CONST_SQU_HALF * this.ay;
 
 			this.ship_R.attr({x: this.x, y: this.y, rotation: this.rotation});
 			this.ship_G.attr({x: this.x, y: this.y, rotation: this.rotation});
@@ -98,21 +95,52 @@ Crafty.c('ship', {
 			this.weap_G.attr({x: this.x, y: this.y, rotation: this.rotation, alpha: this.weap});
 		});
 	},
-	setColor: function(r, g, b) {
+	ship: function(id, r, g, b) {
+		this.id = id;
 		this.ship_R.alpha = r * 0.5;
 		this.ship_G.alpha = g * 0.5;
 		this.ship_B.alpha = b * 0.5;
+
+		return this;
+	},
+	updatePos: function(update) {
+		this.x = update['x'];
+		this.y = update['y'];
+		this.rotation = update['theta'];
+		this.vx = update['vx'];
+		this.vy = update['vy'];
+		this.hull = update['health'];
+		this.engine = update['fuel'];
+		this.weap = update['weapon'];
+		this.kills = update['kills'];
+		this.deaths = update['deaths'];
+
+		return this;
+	},
+	remove: function() {
+		// TODO: handle removal of ship from playing field here
+		this.ship_R.destroy();
+		this.ship_G.destroy();
+		this.ship_B.destroy();
+		this.engine_R.destroy();
+		this.engine_G.destroy();
+		this.hull_R.destroy();
+		this.hull_G.destroy();
+		this.weap_R.destroy();
+		this.weap_G.destroy();
+		this.destroy();
 	}
 });
 
 Crafty.c('player', {
 	init: function () {
-		if (!this.has('ship')) this.addComponent('ship');
-		if(!this.has('Keyboard')) this.addComponent('Keyboard');
+		this.requires('ship, Keyboard, socket');
 
-		this.shoot = true;
+		this.attr({
+			shoot: true
+		});
 
-		this.bind("EnterFrame", function(e) {
+		this.bind('EnterFrame', function(e) {
 			// TODO: drain fuel here and variable turn rate
 			if (this.isDown(CTRL_TURN_CW)) {
 				if (this.isDown(CTRL_TURN_CCW)) {
@@ -138,20 +166,163 @@ Crafty.c('player', {
 
 			this.ax = acc * Math.cos(this.rotation * DEG_TO_RAD);
 			this.ay = acc * Math.sin(this.rotation * DEG_TO_RAD);
+
+			if (e.frame % SHIP_UPDATE_RATE === 0) {
+				this.socket.emit('POS', JSON.stringify({
+					id: this.id,
+					x: this.x,
+					y: this.y,
+					theta: this.rotation,
+					vx: this.vx,
+					vy: this.vy,
+					health: this.hull,
+					fuel: this.engine,
+					weapon: this.weap
+				}));
+			}
 		});
 	}
 });
 
-Crafty.scene('ship_test', function() {
-	// test a ship
-	Crafty.e('ship, player').attr({
-		x: FIELD_WIDTH / 2,
-		y: FIELD_HEIGHT / 2
-	}).setColor(1, 1, 1);
+Crafty.c('bullet', {
+	init: function() {
+		this.requires('2D, DOM');
+
+		this.attr({
+			id: null,
+			h: BULLET_RADIUS,
+			w: BULLET_RADIUS,
+			vx: 0,
+			vy: 0,
+			playerId: null,
+			power: null,
+			bullets: null
+		});
+
+		this.bind('EnterFrame', function(e) {
+			this.vx = this.vx + this.ax * TIME_CONST;
+			this.vy = this.vy + this.ay * TIME_CONST;
+			this.x = this.x + this.vx * TIME_CONST;
+			while (this.x > FIELD_WIDTH) {
+				this.x -= FIELD_WIDTH;
+			}
+			while (this.x < 0) {
+				this.x += FIELD_WIDTH;
+			}
+
+			this.y = this.y + this.vy * TIME_CONST;
+			while (this.y > FIELD_HEIGHT) {
+				this.y -= FIELD_HEIGHT;
+			}
+			while (this.y < 0) {
+				this.y += FIELD_HEIGHT;
+			}
+
+			// TODO: decay bullet and kill itself
+		});
+	},
+	bullet: function(id, playerId, x, y, vx, vy, power, bullets) {
+		this.id = id;
+		this.playerId = playerId;
+		this.x = x;
+		this.y = y;
+		this.vx = vx;
+		this.vy = vy;
+		this.power = power;
+		this.bullets = bullets;
+
+		return this;
+	},
+	remove: function() {
+		delete this.bullets[this.id];
+		this.destroy();
+	}
+});
+
+Crafty.scene('game', function() {
+	var socket = io.connect();
+	var ships = {};
+	var bullets = {};
+
+	socket.on('JOIN', function (data) {
+		console.log(data);
+		var player = Crafty.e('ship, player').initSocket(socket).ship(
+			data['id'],
+			data['color']['r'],
+			data['color']['g'],
+			data['color']['b'])
+		.attr({
+			x: data['x'],
+			y: data['y']
+		});
+
+		ships = {};
+		ships[data['id']] = player;
+
+		var ship;
+		for (ship in data['ships']) {
+			ships[ship['id']] = Crafty.e('ship').ship(
+				ship['id'],
+				ship['color']['r'],
+				ship['color']['g'],
+				ship['color']['b']).updatePos(ship);
+		}
+	});
+
+	socket.on('DEATH', function (data) {
+		console.log(data);
+		// TODO: render the death animation
+	});
+
+	socket.on('POS', function (data) {
+		console.log(data);
+		var ship;
+		for (ship in data) {
+			ships[ship['id']].updatePos(ship);
+		}
+	});
+
+	socket.on('CONN', function (data) {
+		console.log(data);
+		ships[data['id']] = Crafty.e('ship').ship(
+			data['id'],
+			data['color']['r'],
+			data['color']['g'],
+			data['color']['b']
+		).attr({
+			x: data['x'],
+			y: data['y']
+		});
+	});
+
+	socket.on('DISCONN', function (data) {
+		console.log(data);
+		ships[data['id']].remove();
+		delete ships[data['id']];
+	});
+
+	socket.on('BULLET', function (data) {
+		console.log(data);
+		bullets[data['id']] = Crafty.e('bullet').bullet(
+			data['id'],
+			data['playerId'],
+			data['x'],
+			data['y'],
+			data['vx'],
+			data['vy'],
+			data['power'],
+			bullets);
+	});
+
+	socket.on('BULLET_DEATH', function (data) {
+		console.log(data);
+		bullets[data['id']].remove();
+	});
+
 });
 
 window.onload = function() {
 	//start crafty, full screen
 	Crafty.init(WINDOW_WIDTH, WINDOW_HEIGHT);
-	Crafty.scene('ship_test');
+	Crafty.scene('game');
 };
